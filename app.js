4c05e702c9d51e62036ccf725ee248cd6e38795a
JulsVilor <58804803+ArcaneF22@users.noreply.github.com>
2024-06-14 15:37:00 +0800

    sa

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
-const express = require("express");
-const fileUpload = require("express-fileupload");
-const path = require("path");
+import React, { useState } from "react"
+import axios from 'axios'
 
-const filesPayloadExists = require('./middleware/filesPayloadExists');
-const fileExtLimiter = require('./middleware/fileExtLimiter');
-const fileSizeLimiter = require('./middleware/fileSizeLimiter');
+function App() {
+  const [file, setFile] = useState()
+  const upload = () => {
+    const formData = new FormData()
+    formData.append('file', file)
+    axios.post('http://localhost:3001/upload',formData )
+    .then( res => {})
+    .catch(er => console.log(er))
+  }
+   return (
+    <div>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])}/>
+      <button type="button" onClick={upload}>Upload</button>
+    </div>
+  )
+}
 
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.post('/upload',
-    fileUpload({ createParentPath: true }),
-    filesPayloadExists,
-    fileExtLimiter(['.png', '.jpg', '.jpeg', '.svg']),
-    fileSizeLimiter,
-    (req, res) => {
-        const files = req.files
-        console.log(files)
-
-        Object.keys(files).forEach(key => {
-            const filepath = path.join(__dirname, 'files', files[key].name)
-            files[key].mv(filepath, (err) => {
-                if (err) return res.status(500).json({ status: "error", message: err })
-            })
-        })
-
-        return res.json({ status: 'success', message: Object.keys(files).toString() })
-    }
-)
-
-
-
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default App;
